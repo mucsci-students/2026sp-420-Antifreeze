ba45586c951f8326298cf3b4cacd75955956af22
Zane Weaver <zaweaver@millersville.edu>
2026-02-04 21:36:36 -0500

    Fleshed out Skeleton Further

--- src/schedule.py
+++ src/schedule.py
@@ -1,3 +1,4 @@
+import 
 from scheduler import (
     Scheduler,
     load_config_from_file,
@@ -5,3 +6,100 @@ from scheduler import (
 from scheduler.config import CombinedConfig
 
 
+#--------------#
+#FILE MANAGEMENT
+#TODO Implement feature
+def loadFile():
+    return
+
+#TODO Implement feature
+def saveFile():
+    return
+
+#TODO Implement feature
+def printFile():
+    return
+
+
+#--------------#
+#LABS
+#TODO Implement feature
+def addLab():
+    return
+
+#TODO Implement feature
+def deleteLab():
+    return
+
+#TODO Implement feature
+def modifyLab():
+    return
+
+
+#--------------#
+#ROOMS
+#TODO Implement feature
+def addRoom():
+    return
+
+#TODO Implement feature
+def deleteRoom():
+    return
+
+#TODO Implement feature
+def modifyRoom():
+    return
+
+#--------------#
+#CONFLICT
+#TODO Implement feature
+def addConflict():
+    return
+
+#TODO Implement feature
+def deleteConflict():
+    return
+
+#TODO Implement feature
+def modifyConflict():
+    return
+
+#--------------#
+#FACULTY
+#TODO Implement feature
+def addFaculty():
+    return
+
+#TODO Implement feature
+def deleteFaculty():
+    return
+
+#TODO Implement feature
+def modifyFaculty():
+    return
+
+#--------------#
+#COURSE
+#TODO Implement feature
+def addCourse():
+    return
+
+#TODO Implement feature
+def deleteCourse():
+    return
+
+#TODO Implement feature
+def modifyCourse():
+    return
+
+
+#--------------#
+#SCHEDULER
+
+#TODO Implement feature
+def runScheduler():
+    return
+
+#TODO Implement feature
+def displaySchedule():
+    return
