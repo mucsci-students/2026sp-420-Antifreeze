5fe405e447871abb3e8b44c9fead6f32b5defaee
An Nguyen <110743768+pomodokoworks@users.noreply.github.com>
2026-02-14 16:10:31 -0500

    Updated faculty.py for testing

--- src/schedule/faculty.py
+++ src/schedule/faculty.py
@@ -1,6 +1,4 @@
 from scheduler import (
-    Scheduler,
-    load_config_from_file,
     FacultyConfig,
     Faculty,
     Day,
@@ -10,7 +8,6 @@ from scheduler import (
     Room,
     Lab
 )
-from scheduler.config import CombinedConfig
 
 
 class faculty():
@@ -22,31 +19,32 @@ class faculty():
     #Adds a new faculty to the configuration json
     #Parameters: Configuration file, scheduler, name of faculty, maximum credits, maximum days, minimum credits, unique course
                 #limit, times, course preferences, room preferences, lab preferences, mandatory days
-    #Example usage: addFaculty(example.json, )
     def addFaculty(self, config, name: Faculty, maximumCredits: int, maximumDays: int, minimumCredits: int,
                    uniqueCourseLimit: int, times: dict[Day, list[TimeRange]], coursePreferences: dict[Course, Preference],
                    roomPreferences: dict[Room, Preference], labPreferences: dict[Lab, Preference], mandatoryDays: set[Day]):
         #Reference to faculty list inside database
-        faculty = config.config.faculty
+        fac = config.config.faculty
 
         #Test if parameters are correct
         #test = self.testAddFaculty(name, config, maximumCredits, maximumDays, minimumCredits, uniqueCourseLimit,
                                   # times, coursePreferences, roomPreferences, labPreferences, mandatoryDays)
 
         #Checking for duplicate faculty name
-        if name in faculty:
-            print("Faculty already exists - no change made.")
+        for prof in fac:
+            if prof.name == name:
+                print("Faculty already exists - no change made.")
         
-        newMember = FacultyConfig(name=name, maximum_credits=maximumCredits, maximum_days=maximumDays, minimum_credits=minimumCredits, unique_course_limit=uniqueCourseLimit,
-                      times=times, course_preferences=coursePreferences, room_preferences=roomPreferences, lab_preferences=labPreferences, mandatory_days=mandatoryDays)
-        faculty.append(newMember)
+        newMember = FacultyConfig(name = name, maximum_credits = maximumCredits, maximum_days = maximumDays, 
+                                  minimum_credits = minimumCredits, unique_course_limit = uniqueCourseLimit,
+                                  times = times, course_preferences = coursePreferences, room_preferences = roomPreferences, 
+                                  lab_preferences = labPreferences, mandatory_days = mandatoryDays)
+        fac.append(newMember)
         print(f"Faculty member '{name}' added successfully")
     
     #Modify Faculty
     #Modifies the details of an existing faculty member in the configuration json
     #Parameters: Configuration file, scheduler, name of faculty, maximum credits, maximum days, minimum credits, unique course
                 #limit, times, course preferences, room preferences, lab preferences, mandatory days
-    #Example usage: addFaculty(example.json, )
     def modifyFaculty(self, config, name: Faculty, maximumCredits: int, maximumDays: int, minimumCredits: int,
                       uniqueCourseLimit: int, times: dict[Day, list[TimeRange]], coursePreferences: dict[Course, Preference],
                       roomPreferences: dict[Room, Preference], labPreferences: dict[Lab, Preference], mandatoryDays: set[Day]):
@@ -56,12 +54,15 @@ class faculty():
         #Checking to see if faculty exists under provided name
         for prof in fac:
             if prof.name == name:
-                self.deleteFaculty(config, name)
-                self.addFaculty(config=config, name=name, maximumCredits=maximumCredits, maximumDays=maximumDays, minimumCredits=minimumCredits, uniqueCourseLimit=uniqueCourseLimit,
-                                times=times, coursePreferences=coursePreferences, roomPreferences=roomPreferences, labPreferences=labPreferences, mandatoryDays=mandatoryDays)
+                fac.remove(prof)
+                newMember = FacultyConfig(name = name, maximum_credits = maximumCredits, maximum_days = maximumDays, 
+                                          minimum_credits = minimumCredits, unique_course_limit = uniqueCourseLimit,
+                                          times = times, course_preferences = coursePreferences, room_preferences = roomPreferences, 
+                                          lab_preferences = labPreferences, mandatory_days = mandatoryDays)
+                fac.append(newMember)
+                print(f"Faculty member '{name}' modified successfully")
                 return
 
-                 
         print("No such faculty member exists.")
         return
     
@@ -69,20 +70,22 @@ class faculty():
     #Delete Faculty
     #Delete an existing faculty from the configuration json
     #Parameters: Configuration file, name of faculty
-    #Example usage: deleteFaculty(example.json, "Hogg")
     def deleteFaculty(self, config, name: Faculty):
         
         #Reference to faculty list inside database
-        faculty = config.config.faculty
+        fac = config.config.faculty
 
         #Checking to see if faculty exists under provided name
-        for prof in faculty:
+        for prof in fac:
             if prof.name == name:
-                faculty.remove(prof)
+                fac.remove(prof)
                 print(f"Faculty member '{name}' deleted successfully")
                 return
         print("No such faculty member exists.")
 
+    #Print Faculty
+    #Print the faculty config from the configuration json
+    #Parameters: Configuration file
     def printFaculty(self, config):
         faculty = config.config.faculty
         print("\nFaculty:")
