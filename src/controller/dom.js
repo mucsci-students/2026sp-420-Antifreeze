c6fdc6fac479e155f8cee8ed311fe43adb74071a
Zane Weaver <zaweaver@millersville.edu>
2026-03-23 18:27:57 -0400

    started refactor

---
+++ src/controller/dom.js
@@ -0,0 +1,55 @@
+// ===============================
+// DOM ELEMENTS
+// ===============================
+/
+// Wrapper
+export const wrapper = document.getElementById("wrapper");
+
+// Navigation Buttons
+export const load_button = document.getElementById("load-button");
+export const save_button = document.getElementById("save-button");
+export const back_button = document.getElementById("back-button");
+export const forward_button = document.getElementById("forward-button");
+export const add_button = document.getElementById("add-button");
+export const modify_button = document.getElementById("modify-button");
+export const delete_button = document.getElementById("delete-button");
+export const view_button = document.getElementById("view-button");
+export const print_button = document.getElementById("print-button");
+
+// Field Buttons
+export const faculty_button = document.getElementById("faculty-button");
+export const courses_button = document.getElementById("courses-button");
+export const labs_button = document.getElementById("labs-button");
+export const rooms_button = document.getElementById("rooms-button");
+export const schedule_button = document.getElementById("schedule-button");
+
+// Images inside buttons
+export const back_img = back_button.querySelector("img");
+export const forward_img = forward_button.querySelector("img");
+export const add_img = add_button.querySelector("img");
+export const modify_img = modify_button.querySelector("img");
+export const delete_img = delete_button.querySelector("img");
+export const view_img = view_button.querySelector("img");
+export const print_img = print_button.querySelector("img");
+
+// Layout elements
+export const navigator_div = document.querySelector(".navigator");
+export const main = document.querySelector(".main");
+
+export const nav_bar = document.querySelector(".nav-bar");
+export const top_bar = document.querySelector(".top-bar");
+export const team_name = document.querySelector(".team-name");
+export const gui_wrapper = document.getElementById("wrapper");
+export const config_name = document.getElementById("config-name");
+
+// Popup elements
+export const amd_popup = document.getElementById("amd-popup");
+export const popup_save = document.getElementById("popup-save");
+export const popup_title = document.getElementById("popup-title");
+export const popup_form = document.getElementById("popup-form");
+export const popup_close = document.getElementById("popup-close");
+export const popup_box = document.querySelector(".popup-box");
+export const popup_header = document.getElementById("popup-header");
+
+// File input
+export const file_input = document.getElementById("load");
